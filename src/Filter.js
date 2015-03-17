7471977dfb3d4de2fa8761575a8d01f9bed9c803
charlie <h.uematsu1015@gmail.com>
2015-03-17 18:28:37 +0900

    Added filter function

--- src/Filter.js
+++ src/Filter.js
@@ -1,40 +1,39 @@
-/** @jsx React.DOM */
-
 var _         = require('lodash');
 var React     = require('react');
 var Bootstrap = require('react-bootstrap');
 var Input     = Bootstrap.Input;
-var Panel     = Bootstrap.Panel;
 var Row       = Bootstrap.Row;
 var Col       = Bootstrap.Col;
 
 /**
- * Simple Filter
  * @class Filter
- * @param panel {Boolean} enclose with Bootstrap.Panel
- * @param onChange {Function} call when input values are changed.
- * @param filters {Array} array of filter specific
- *  // filter specific
- *  {
- *    key         : {String}, // filter key
- *    type        : {String}, // filter type // 'string'|'number'|'date'
- *    placeholder : {String}  // input placeholder
- *
- *  }
+ * @param data {Array} objects in array
+ * @param onChange {function} call when filter data is changed
+ * @param filterOpts {Object} filter options
+ * filterOpts = {
+ *  key1: {
+ *    type        : {String} {'text'|'number'|'date'} filter data type is required
+ *    xs          : {Number} Bootstrap Input width
+ *    placeholder : {String} Input placeholder
+ *    perfect     : {Boolean} filter match perfectlly or not
+ *  },
+ *  key2: ...
+ * }
  */
+
 var Filter = React.createClass({
 
   propTypes: {
-    panel    : React.PropTypes.bool,
-    onChange : React.PropTypes.func,
-    filters  : React.PropTypes.array
+    data       : React.PropTypes.array,
+    filterOpts : React.PropTypes.object,
+    onChange   : React.PropTypes.func
   },
 
   getDefaultProps: function() {
     return {
-      panel   : false,
-      onChange: function(){},
-      filters : []
+      data       : [],
+      filterOpts : {},
+      onChange   : function(){}
     }
   },
 
@@ -45,60 +44,91 @@ var Filter = React.createClass({
   },
 
   onChange: function(e) {
-    var name  = e.target.name;
+    var key   = e.target.name;
     var value = e.target.value;
-    var state = this.state.filters;
-    state[name] = value;
-    this.setState(state);
+    var opts = this.props.filterOpts;
+    var filters = this.state.filters;
+
+    filters[key] = value;
 
     // compact
-    var filters = _.reduce(state, function(ret, value, key) {
-      if (value) ret[key] = value;
+    var _filters = _.reduce(filters, function(ret, val, key) {
+      if (val) {
+        var _opts = opts[key];
+        ret[key] = (_opts.type === 'date') ? new Date(val) : val;
+      }
       return ret;
     }, {});
 
+    this.setState({filters: filters});
+    this.filter(_filters);
+  },
+
+  filter: function(filters) {
+    var data = this.props.data;
+    var opts = this.props.filterOpts;
+
+    var list = _.filter(data, function(row) {
+      return _.every(row, function(val, key) {
+
+        // no filter
+        if (!filters[key]) return true;
+
+        var _opts   = opts[key];
+        var type    = _opts.type;
+        var perfect = _opts.perfect;
+        var filterVal = filters[key];
+
+        if (perfect) {
+          return (val === filterVal);
+        } else if (type === 'text') {
+          return val.match(filterVal);
+        } else if (type === 'number') {
+          return val.toString().match(filterVal);
+        } else if (type === 'date') {
+          return (filterVal.getMonth() === val.getMonth() &&
+                  filterVal.getYear() === val.getYear() &&
+                  filterVal.getDate() === val.getDate()
+                 );
+        } else {
+          return (val == filterVal);
+        }
+      });
+    });
+
     // bubble to parent
-    this.props.onChange(filters);
+    this.props.onChange(list);
   },
 
   render: function() {
-    var self = this;
-    var filters = self.props.filters.map(function(filter, index) {
-      var key  = filter.key || index;
-      var type = filter.type || 'text';
-      var xs   = filter.xs || (type === 'date') ? 3 : 2;
-      var placeholder = filter.placeholder || key;
+    var filters = _.map(this.props.filterOpts, function(opts, key) {
+      var type = opts.type || 'text';
+      var xs   = opts.xs || ((type === 'date') ? 3 : 2);
+      var offset = opts.offset || 0;
+      var placeholder = opts.placeholder || key;
+
       return (
-        <Col key={key} xs={xs}>
+        <Col key={key} xs={xs} xsOffset={offset}>
           <Input
             name={key}
             placeholder={placeholder}
             type={type}
-            onChange={self.onChange}
-            value={self.state.filters[key]}
+            onChange={this.onChange}
+            value={this.state.filters[key]}
           />
         </Col>
       );
-    });
-
-    if (this.props.panel) {
-      return (
-        <Panel header='Filter'>
-          <Row>
-            {filters}
-          </Row>
-        </Panel>
-      );
-    }
+    }, this);
 
     return (
-      <Input wrapperClassName="wrapper">
+      <div className={this.props.className}>
         <Row>
           {filters}
         </Row>
-      </Input>
+      </div>
     );
   }
 });
 
 module.exports = Filter;
+
